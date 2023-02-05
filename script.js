871074d0c30990389fdedf314f33eb7622648596
Ashish Deshmukh <115146337+AshishDeshmukh02@users.noreply.github.com>
2023-02-05 16:05:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 //your code here
+const input = document.querySelector("#newTodoInput");
+const button = document.querySelector("#addTodoBtn");
+const list = document.querySelector("#todoList");
 
+button.addEventListener("click", function () {
+  if (!input.value) return; // check if input is empty
+  
+  const newTodo = document.createElement("li");
+  newTodo.textContent = input.value;
+  list.appendChild(newTodo);
+
+  input.value = ""; // reset input value
+});
